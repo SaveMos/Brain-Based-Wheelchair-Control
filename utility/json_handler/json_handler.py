11f1f768e50aa2116fb18020f75901282249bc36
Saverio <158563091+SaveMos@users.noreply.github.com>
2024-12-18 18:34:34 +0100

    Feature: I improved the already Stop&Go  in the orchestrator. Added some functionalities to JSONHandler

--- utility/json_handler/json_handler.py
+++ utility/json_handler/json_handler.py
@@ -1,4 +1,8 @@
 import json
+import logging
+from typing import Any
+
+import jsonschema
 
 class JsonHandler:
     """
@@ -81,7 +85,7 @@ class JsonHandler:
         """
         return self.read_json_file(file_path).get(field_name, None)
 
-    def get_system_address(self , json_filepath: str, system_name: str) -> dict:
+    def get_system_address(self , json_filepath: str, system_name: str) -> Any | None:
         """
         Reads the IP address and port of a specified system from a JSON file.
 
@@ -115,6 +119,34 @@ class JsonHandler:
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
             return None
+
+    def validate_json(self ,json_data: dict, schema_path: str) -> bool:
+        """
+            Validate a json object against a json schema in a file.
+            :param json_data: json object
+            :param schema_path: path to the json schema relative to the data folder
+            :return: True if json object is valid, False otherwise
+            """
+        with open(schema_path, "r", encoding="UTF-8") as file:
+            json_schema = json.load(file)
+        try:
+            jsonschema.validate(instance=json_data, schema=json_schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            logging.error(ex)
+            return False
+        return True
+
+    def validate_json_from_path(self , json_path: str, schema_path: str) -> bool:
+        """
+        Validate a json file against a json schema in a file.
+        :param json_path: file containing the json object
+        :param schema_path: file containing the json schema
+        :return: True if json object is valid, False otherwise
+        """
+        with open(json_path, "r", encoding="UTF-8") as file:
+            json_data = json.load(file)
+        return self.validate_json(json_data, schema_path)
+
 
 # Example to test the class
 if __name__ == "__main__":
