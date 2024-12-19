876153cfff3c25874414f315df93ddf512a987f5
Alessandro <alexasca99@gmail.com>
2024-12-19 10:54:16 +0100

    Fixed in production orchestrator the issue with calling of receive and send functions.

--- production_system/production_orchestrator.py
+++ production_system/production_orchestrator.py
@@ -1,6 +1,6 @@
 from production_system.configuration_parameters import ConfigurationParameters
 from production_system.classifier_deployment import ClassifierDeployment
-from production_system.production_system_jsonio import ProductionSystemJSONIO
+from production_system.production_system_io import ProductionSystemIO
 from production_system.classification import Classification
 
 
@@ -22,26 +22,28 @@ class ProductionOrchestrator:
         configuration = ConfigurationParameters()
         configuration.get_config_params()
 
-        #receive classifier
-        prod_sys_io = ProductionSystemJSONIO()
-        classifier = prod_sys_io.receive_classifier()
+        # receive classifier or prepared session
+        prod_sys_io = ProductionSystemIO()
+        sender, message = prod_sys_io.get_last_message()
 
-        deployment = ClassifierDeployment()
-        deployment.deploy(classifier)
+        if sender == "Develop" :
+            classifier = message
+            deployment = ClassifierDeployment()
+            deployment.deploy(classifier)
 
-        #receive prepared session
-        prod_sys_io = ProductionSystemJSONIO()
-        prepared_session = prod_sys_io.receive_prepared_session()
-
-        #classify operation
-        classification = Classification()
-        label = classification.classify(prepared_session)
+        elif sender == "Preparation" :
+            #classify operation
+            prepared_session = message
+            classification = Classification()
+            label = classification.classify(prepared_session)
 
         #if evaluation phase parameter is true label is sent also to Evaluation System
-        prod_sys_io = ProductionSystemJSONIO()
+        prod_sys_io = ProductionSystemIO()
         if configuration.evaluation_phase:
             configuration.evaluation_phase = False
-            prod_sys_io.send_label(label) #da sistemare
+            target_ip = ConfigurationParameters.EVALUATION_SYSTEM_IP
+            target_port = ConfigurationParameters.EVALUATION_SYSTEM_PORT
+            prod_sys_io.send_message(target_ip, target_port, label)
 
         # Send label to client
-        prod_sys_io.send_label(label) # da sistemare
+        #prod_sys_io.send_message(label) # da sistemare
